118e62f6f0e3e7f46fcd1956779dde07b40aa91c
Amit <amit.dikkar@gmail.com>
2014-03-30 03:57:27 -0700

    Twitter Bootstrap Template Working Successfully
    Special offer, Normal, Contact Pages are working.
    Adding Register and Login pages in UserAccount Controller is in
    progress.

---
+++ MvcApplication2/MvcApplication2/App_Start/BundleConfig.cs
@@ -0,0 +1,50 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MvcApplication2
+{
+    public class BundleConfig
+    {
+        /*
+         Instruction: add all js and css files in bundles
+         */
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear();
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        //"~/Scripts/jquery-{version}.js",
+                        //"~/Scripts/bootstrap.js",
+                        //"~/Scripts/bootstrap.min.js",
+                        "~/Content/themes/js/jquery.js",
+                        "~/Content/themes/js/bootstrap.min.js",
+                        "~/Content/themes/js/google-code-prettify/prettify.js",
+                        "~/Content/themes/js/bootshop.js",
+                        "~/Content/themes/js/jquery.lightbox-0.5.js",
+                        "~/Content/themes/switch/theamswitcher.js"));
+
+            
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                //"~/Content/bootstrap.min.css",
+                "~/Content/bootstrap.css",
+                "~/Content/themes/bootshop/bootstrap.min.css",
+                "~/Content/themes/css/base.css",
+                 "~/Content/themes/css/bootstrap-responsive.min.css",
+                "~/Content/themes/css/font-awesome.css",
+                "~/Content/themes/js/google-code-prettify/prettify.css",
+                "~/Content/themes/switch/themeswitch.css"));
+        }
+    }
+}
---
+++ MvcApplication2/MvcApplication2/Controllers/UserAccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        //
+        // GET: /UserAccount/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /UserAccount/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //
+        // GET: /UserAccount/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
