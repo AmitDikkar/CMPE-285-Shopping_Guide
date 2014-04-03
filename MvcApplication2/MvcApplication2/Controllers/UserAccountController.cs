b0740530f6cfa7dfa7b99582eb251a767ce61bc2
Amit <amit.dikkar@gmail.com>
2014-04-02 18:10:24 -0700

    Second commit
    Worked on Models, Able to store data in local db but yet to store in SQL
    server. Working on Registration part.

--- MvcApplication2/MvcApplication2/Controllers/UserAccountController.cs
+++ MvcApplication2/MvcApplication2/Controllers/UserAccountController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcApplication2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace MvcApplication2.Controllers
 {
     public class UserAccountController : Controller
     {
+        public UserDbContext userDbContext = new UserDbContext();
+        
         //
         // GET: /UserAccount/
 
@@ -21,6 +24,30 @@ namespace MvcApplication2.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        //
+        // POST: /UserAccount/Login
+        [HttpPost]
+        public ActionResult Login(User existingUser)
+        {
+            return View();
+        }
+
+        //
+        // POST: /UserAccount/Register
+        [HttpPost]
+        public ActionResult Register(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                userDbContext.users.Add(newUser);
+                userDbContext.SaveChanges();
+            }
+
+            //TODO: Add view bag message to show Thank you message or sorry already exist message
+            //Change this to redirect to same page "Index" method.
+            return RedirectToAction("Index","Home");
         }
 
         //
---
+++ MvcApplication2/MvcApplication2/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcApplication2.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> users { get; set; }
+    }
+}
